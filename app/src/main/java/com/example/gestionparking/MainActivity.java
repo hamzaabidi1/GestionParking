cf0f8544d7f19b714e02f4497be3458da8fcbea9
hamza <hamza.abidi1@esprit.tn>
2021-10-26 23:43:04 +0100

    lastest update for main

--- app/src/main/java/com/example/gestionparking/MainActivity.java
+++ app/src/main/java/com/example/gestionparking/MainActivity.java
@@ -3,45 +3,22 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.view.View;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.google.android.material.button.MaterialButton;
 
 public class MainActivity extends AppCompatActivity {
-    private TextView signup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        TextView signup;
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         signup=findViewById(R.id.signintxt);
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Intent intent=new Intent(MainActivity.this,SignupActivity.class);
-                startActivity(intent);
-            }
+        signup.setOnClickListener(view -> {
+            Intent intent=new Intent(MainActivity.this,SignupActivity.class);
+            startActivity(intent);
         });
 
-        TextView username =(TextView) findViewById(R.id.Brand);
-        TextView password =(TextView) findViewById(R.id.Serie);
-
-        MaterialButton loginbtn = (MaterialButton) findViewById(R.id.loginbtn);
-
-        //admin and admin
-
-        loginbtn.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                if(username.getText().toString().equals("admin") && password.getText().toString().equals("admin")){
-                    //correct
-                    Toast.makeText(MainActivity.this,"LOGIN SUCCESSFUL",Toast.LENGTH_SHORT).show();
-                }else
-                    //incorrect
-                    Toast.makeText(MainActivity.this,"LOGIN FAILED !!!",Toast.LENGTH_SHORT).show();
-            }
-        });
 
 
     }
